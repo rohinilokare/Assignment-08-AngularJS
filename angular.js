1fb8f983f4a7b87a76f6bd45c63c0d3eef7ae8b3
rohinilokare <rohini@treeni.com>
2019-07-05 15:44:27 +0530

    feature::added service for increment and decrement operation

--- angular.js
+++ angular.js
@@ -1,39 +1,55 @@
+count = 0
 var mainMod = angular.module('ThingCounterApp', []);
 
+// mainMod.controller('myCtrl',function($scope) {
+//     //$scope.showpage = true;
+//     $scope.label = 'rohiniCounter';
+//     $scope.color = 'yellow'
 
-mainMod.controller('myCtrl',function($scope) {
-    $scope.showpage = true;
+// });
+
+mainMod.service("CounterService", function(){
+  this.increment = function(){
+    count =count + 1;
+    return count;
+  };
+
+  this.decrement = function(){
+    count = count -1;
+    return count;
+  };
+
+  this.label = 'rohiniCounter';
+  console.log('in label service');
+  this.color = 'yellow';
+
 });
 
+mainMod.controller('myCtrl',function($scope,CounterService ) {
+  $scope.increment = function(){
+    $scope.count = CounterService.increment();
+  }
 
-mainMod.controller('MainCtrl', function ($scope) {
-    $scope.text = '';
+  $scope.decrement = function(){
+    $scope.count = CounterService.decrement();
+  }
+
+  $scope.label = CounterService.label;
+
+  $scope.color = CounterService.color;
+
 });
 
-mainMod.directive('myCustomer', function() {
+mainMod.directive('myCounter', function() {
   return {
-    // template : '<h1>Hello</h1><table style="border:white 1px solid"><tr><td style="border-right:1px white solid">name</td><td>Roll No</td><td>std</td></tr></table>'
-    template : '<h1>ActiveCounterList</h1><div class="button_group"><button style="border:white 1px solid">-</button><button>label</button><button>count</button><button>+</button></div>'
+    template : '<h1>ActiveCounterList</h1><div style="color:{{color}}"class="button_group"><button ng-click="decrement()"><span class = "minus"> - </span></button><button>{{label}}</button><button>{{count}}</button><button ng-click="increment()"><span class = "plus"> + </span></button></div>'
   };
 });
 
 
+mainMod.addCounter = function(label,color){
+  this.label = label;
+  this.color = color
+console.log(this.label);
 
-
-function validate() {
-  if( document.myForm.lable.value == "" ) {
-     alert( "Please provide your lable!" );
-     document.myForm.lable.focus() ;
-     return false;
-  }else{
-    label = document.myForm.lable.value
-    console.log(lable)
-    document.getElementById("countername").innerHTML = "lable";
-  }
-  if( document.myForm.color.value == "" ) {
-     alert( "Please provide your color!" );
-     document.myForm.color.focus() ;
-     return false;
-  }
-   return( true );
 }
