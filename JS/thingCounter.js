acb5a3e729e38c4bd3527ce95bd80905adef0951
rohinilokare <rohini@treeni.com>
2019-07-07 00:10:42 +0530

    created multiple buttons by using static data

--- JS/thingCounter.js
+++ JS/thingCounter.js
@@ -1,31 +1,50 @@
 var mainMod = angular.module('ThingCounterApp', []);
 
-mainMod.controller('myCtrl',[function($scope) {
-  var self = this ;
-  self.label = 'label';
-  self.color = 'yellow';
-  self.submit = function() {
-        console.log('User clicked submit with ',
-            self.label, self.color);
-      };
+mainMod.controller('myCtrl',function($scope) {
+	    $scope.records = [
+       {
+            "label" : "counter1",
+            "color" : "pink"
+        },{
+            "label" : "counter2",
+            "color" : "green"
+        },{
+            "label" : "counter3",
+            "color" : "white"
+        },{
+            "label" : "counter4",
+            "color" : "yellow"
+        }
+    ]
 
-}]);
+  // var self = this ;
+  // self.label = 'label';
+  // self.color = 'yellow';
+  // self.submit = function() {
+  //       console.log('User clicked submit with ',
+  //           self.label, self.color);
+  //     };
+  			 // $scope.label = []
+  			 // $scope.label.push('rohini');
+      // $scope.label = new Array;
+      // $scope.label.push(label);
+});
 
 mainMod.directive('myCounter', function() {
   return {
-    template : '<div class="button_group"><button ng-click="decrement()" style="background-color:{{color}}"><span class = "minus"> - </span></button><button style="background-color:{{color}}">label:{{label}}</button><button style="background-color:{{color}}">count:{{count}}</button><button ng-click="increment()" style="background-color:{{color}}"><span class = "plus"> + </span></button></div><div></div>',
+    template : '<div ng-repeat="x in records"><div class="button_group"><button ng-click="decrement()" style="background-color:{{x.color}}"><span class = "minus"> - </span></button><button style="background-color:{{x.color}}">label :{{ x.label }}</button><button style="background-color:{{x.color}}">count:{{count}}</button><button ng-click="increment()" style="background-color:{{x.color}}"><span class = "plus"> + </span></button></div><div class="space-for-buttons"></div></div>',
     restrict: 'E',
     link:function(scope,elm,attr) {
-       scope.count = 0;
+       scope.count = 0
+
        scope.increment = function(){
-       scope.count = scope.count + 1
-       return scope.count;
+       scope.count ++ ;
        }
 
       scope.decrement = function(){
-        scope.count = scope.count - 1;
-        return scope.count;
-    }
+        scope.count -- ;
+    	}
+
     }
   };
 });
