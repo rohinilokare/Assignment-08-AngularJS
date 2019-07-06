e4e64422224af43f1a241a184a1d690d5d153570
rohinilokare <rohini@treeni.com>
2019-07-07 03:12:07 +0530

    feature::add button group dynamically

--- JS/thingCounter.js
+++ JS/thingCounter.js
@@ -1,49 +1,52 @@
 var mainMod = angular.module('ThingCounterApp', []);
 
-mainMod.controller('myCtrl',function($scope) {
-	    $scope.records = [
-       {
-            "label" : "counter1",
-            "color" : "pink"
-        },{
-            "label" : "counter2",
-            "color" : "green"
-        },{
-            "label" : "counter3",
-            "color" : "white"
-        },{
-            "label" : "counter4",
-            "color" : "yellow"
-        }
-    ]
+mainMod.service('CounterService',function(){
+  this.addCounter = function($scope){
+    var counter = {
+      initial_count: 0,
+      label:$scope.label,
+      color: $scope.color
+    }
+    $scope.counters.push(counter)
+    console.log($scope.counters)
+  }
+});
 
-  // var self = this ;
-  // self.label = 'label';
-  // self.color = 'yellow';
-  // self.submit = function() {
-  //       console.log('User clicked submit with ',
-  //           self.label, self.color);
-  //     };
-  			 // $scope.label = []
-  			 // $scope.label.push('rohini');
-      // $scope.label = new Array;
-      // $scope.label.push(label);
+mainMod.controller('counterController',function($scope,CounterService) {
+  $scope.showForm = function(){
+      $scope.hideform = true;
+  }
+
+  $scope.SubmitForm = function()
+  {
+    console.log('in submit form')
+    CounterService.addCounter($scope)
+    refreshForm();
+  }
+
+  function refreshForm()
+    {
+      $scope.color = '';
+      $scope.label = '';
+      $scope.hideform = false;
+    }
+
+    $scope.counters = [];
 });
 
 mainMod.directive('myCounter', function() {
   return {
-    template : '<div ng-repeat="x in records"><div class="button_group"><button ng-click="decrement()" style="background-color:{{x.color}}"><span class = "minus"> - </span></button><button style="background-color:{{x.color}}">label :{{ x.label }}</button><button style="background-color:{{x.color}}">count:{{count}}</button><button ng-click="increment()" style="background-color:{{x.color}}"><span class = "plus"> + </span></button></div><div class="space-for-buttons"></div></div>',
+    template : '<div ng-repeat="counter in counters track by $index"><div class="button_group"><button ng-click="decrement($index)" style="background-color:{{counter.color}}"><span class = "minus"> - </span></button><button style="background-color:{{counter.color}}">label :{{ counter.label }}</button><button style="background-color:{{counter.color}}">count:{{counter.initial_count}}</button><button ng-click="increment($index)" style="background-color:{{counter.color}}"><span class = "plus"> + </span></button></div><div class="space-for-buttons"></div></div>',
     restrict: 'E',
-    link:function(scope,elm,attr) {
-       scope.count = 0
+    link:function($scope,elm,attr) {
 
-       scope.increment = function(){
-       scope.count ++ ;
+       $scope.increment = function(index_of_counter_array){
+       $scope.counters[index_of_counter_array].initial_count ++ ;
        }
 
-      scope.decrement = function(){
-        scope.count -- ;
-    	}
+      $scope.decrement = function(index_of_counter_array){
+        $scope.counters[index_of_counter_array].initial_count -- ;
+      }
 
     }
   };
