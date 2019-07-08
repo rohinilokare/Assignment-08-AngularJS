9b48c723f6d461b4f389eb976c5e8f16b4fc9962
rohinilokare <rohini@treeni.com>
2019-07-08 12:43:56 +0530

    Angular JS:indentation modification

--- JS/thingCounter.js
+++ JS/thingCounter.js
@@ -7,48 +7,45 @@ mainMod.service('CounterService',function(){
       label:$scope.label,
       color: $scope.color
     }
-    $scope.counters.push(counter)
-    console.log($scope.counters)
+  $scope.counters.push(counter)
   }
 });
 
 mainMod.controller('counterController',function($scope,CounterService) {
-$scope.color = '';
-$scope.label = 'default';
+  $scope.color = '';
+  $scope.label = 'default';
 
   $scope.submitForm = function()
   {
-    var form = myForm.label.$valid
-    console.log(form)
-    console.log('in submit form')
     CounterService.addCounter($scope)
     refreshForm();
   }
 
   function refreshForm()
-    {
-      $scope.color = '';
-      $scope.label = 'default';
-      $scope.showform = false;
-    }
+  {
+    $scope.color = '';
+    $scope.label = 'default';
+    $scope.showform = false;
+  }
 
-    $scope.counters = [];
+  $scope.counters = [];
 });
 
 mainMod.directive('myCounter', function() {
   return {
     template : '<div ng-repeat="counter in counters track by $index"><div class="button_group"><button ng-click="decrement($index)" style="background-color:{{counter.color}}"><span class = "minus"> - </span></button><button style="background-color:{{counter.color}}">label :{{ counter.label }}</button><button style="background-color:{{counter.color}}">count:{{counter.initial_count}}</button><button ng-click="increment($index)" style="background-color:{{counter.color}}"><span class = "plus"> + </span></button></div><div class="space-for-buttons"></div></div>',
+
     restrict: 'E',
+
     link:function($scope,elm,attr) {
 
-       $scope.increment = function(index_of_counter_array){
-       $scope.counters[index_of_counter_array].initial_count ++ ;
-       }
+      $scope.increment = function(index_of_counter_array){
+        $scope.counters[index_of_counter_array].initial_count ++ ;
+      }
 
       $scope.decrement = function(index_of_counter_array){
         $scope.counters[index_of_counter_array].initial_count -- ;
       }
-
     }
   };
 });
