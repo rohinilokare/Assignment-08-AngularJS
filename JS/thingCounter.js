332e9501cd965960d939e48f55a307d3840557d8
rohinilokare <rohini@treeni.com>
2019-07-31 18:59:48 +0530

    feature :: AngularJS :: Redesign HTML

--- JS/thingCounter.js
+++ JS/thingCounter.js
@@ -1,44 +1,40 @@
 var mainMod = angular.module('ThingCounterApp', []);
 
-mainMod.service('CounterService',function(){
+mainMod.service('CounterService',function(){    //reusable business logic independent of views
   this.addCounter = function($scope){
-    var counter = {
-      initial_count: 0,
-      label:$scope.label,
-      color: $scope.color
-    }
+    var counter = {initial_count: 0,label:$scope.label,color:$scope.color}
   $scope.counters.push(counter)
   }
 });
 
+//The business logic behind views
 mainMod.controller('counterController',function($scope,CounterService) {
-  $scope.color = '';
+  $scope.color = '#c9b5b5';
   $scope.label = 'default';
 
   $scope.submitForm = function()
   {
     CounterService.addCounter($scope)
     refreshForm();
+     $scope.showform=false;
   }
 
   function refreshForm()
   {
-    $scope.color = '';
+    console.log('in refresh form')
+    $scope.color = '#c9b5b5';
     $scope.label = 'default';
-    $scope.showform = false;
   }
 
   $scope.counters = [];
 });
 
-mainMod.directive('myCounter', function() {
+//Extend HTML with custom attribute & element
+mainMod.directive('myCounter',function() {
   return {
-    template : '<div ng-repeat="counter in counters track by $index"><div class="button_group"><button ng-click="decrement($index)" style="background-color:{{counter.color}}"><span class = "minus"> - </span></button><button style="background-color:{{counter.color}}">label :{{ counter.label }}</button><button style="background-color:{{counter.color}}">count:{{counter.initial_count}}</button><button ng-click="increment($index)" style="background-color:{{counter.color}}"><span class = "plus"> + </span></button></div><div class="space-for-buttons"></div></div>',
-
     restrict: 'E',
 
     link:function($scope,elm,attr) {
-
       $scope.increment = function(index_of_counter_array){
         $scope.counters[index_of_counter_array].initial_count ++ ;
       }
@@ -46,6 +42,14 @@ mainMod.directive('myCounter', function() {
       $scope.decrement = function(index_of_counter_array){
         $scope.counters[index_of_counter_array].initial_count -- ;
       }
+
+      $scope.updateLabel = function(index_of_counter_array){
+        $scope.counters[index_of_counter_array].label=$scope.label
+      }
+      $scope.invalidObject ={
+      "color":"red"
+      }
+
     }
   };
 });
